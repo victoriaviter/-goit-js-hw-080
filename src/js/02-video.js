cf42d36810d05c9d27514e0407c285e4c90245a0
victoriaviter <viktoriagrychyna@gmail.com>
2022-01-11 23:02:51 +0200

    com1

---
+++ src/js/02-video.js
@@ -0,0 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle'; 
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+document.addEventListener('play',throttle(videoStart, 1000))
+
+
+player.on('timeupdate', throttle(() => {
+    player.getCurrentTime().then( (sec) => {
+        console.log('title:', sec);
+        localStorage.setItem('current-time', JSON.stringify(sec))
+    });
+}, 1000),
+);
+   
+const toStart = JSON.parse(localStorage.getItem('current-time')) || 0;
+console.log(toStart);
+player.setCurrentTime(toStart);
+
+function videoStart(toStart) {
+    console.log(toStart);
+    
+}
